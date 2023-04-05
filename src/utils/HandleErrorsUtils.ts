80040e74608bbe0199c427962c33d7626499b381
eduardor0cha <c.eduardo_rn@outlook.com>
2023-04-05 17:19:05 -0300

    Update Prisma ORM

--- src/utils/HandleErrorsUtils.ts
+++ src/utils/HandleErrorsUtils.ts
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 export type ErrorResponse = {
   code: number;
